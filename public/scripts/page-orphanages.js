cc524d9b34109c738c83639258bf57b8bc1cca5f
Lucas4lmeida <lucasalmeidasilveira16@gmail.com>
2023-06-29 22:58:53 -0300

    modified:   orphanages.html 	modified:   public/css/page-orphanages.css 	modified:   public/scripts/page-orphanages.js

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,1 +1,16 @@
-const map = L.map('mapid').setView([51.505, -0.09], 13);
+//Tipos de dados
+//String ""
+// Number 0123
+// Object {}
+// Boolean true or false
+//Array []
+const map = L.map("map").setView([51.505, -0.09], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+const marker = L.marker([51.5, -0.09]).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
