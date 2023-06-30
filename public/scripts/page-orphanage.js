1d5d41216575740e1a922e8b15fe2472a8fb92b4
Lucas4lmeida <lucasalmeidasilveira16@gmail.com>
2023-06-30 05:20:42 -0300

    modified:   orphanage.html 	modified:   public/css/page-orphanage.css 	new file:   public/scripts/page-orphanage.js

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,28 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+// Criando o mapa
+const map = L.map("map", options).setView([-3.7749729,-38.5753415], 14);
+
+// Adicionando o mapa ao meu site
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+// Criando o Icone
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+// Criando o marcador
+L
+.marker([-3.7749729,-38.5753415], { icon })
+.addTo(map)
+
+
