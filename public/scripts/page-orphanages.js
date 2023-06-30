809b652c9dc0f8317d73f3a73886f87843319bfc
Lucas4lmeida <lucasalmeidasilveira16@gmail.com>
2023-06-30 00:32:59 -0300

    modified:   orphanages.html 	modified:   public/css/animations.css 	modified:   public/css/page-orphanages.css 	modified:   public/scripts/page-orphanages.js

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -4,13 +4,33 @@
 // Object {}
 // Boolean true or false
 //Array []
-const map = L.map("map").setView([51.505, -0.09], 13);
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
+// Criando o mapa
+const map = L.map("map").setView([-3.7910897,-38.5513984], 12);
 
-const marker = L.marker([51.5, -0.09]).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+// Adicionando o mapa ao meu site
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+// Criando o Icone
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+// Criando o Popup
+const popup = L.popup({
+    closeButton: false,
+    className:'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Abrigo Tia Júlia <a href="orphanages.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg"></a>')
+
+// Criando o marcador
+L
+.marker([-3.7749588,-38.6048183], { icon })
+.addTo(map)
+.bindPopup(popup)
+
+
